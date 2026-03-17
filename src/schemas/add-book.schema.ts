86293f41e695aafcecf644f0bf0d64d9b906a935
LuizDev7727 <luiz.antonioq2003@gmail.com>
2026-03-17 16:53:55 -0300

    chore: change book status value on select on form

--- src/schemas/add-book.schema.ts
+++ src/schemas/add-book.schema.ts
@@ -4,7 +4,7 @@ export const addBookSchema = z.object({
   title: z.string(),
   author: z.string(),
   imageUrl: z.url({ error: "URL Inválida" }).optional(),
-  status: z.enum(["Lido", "Lendo", "Quero Ler"], { error: "Status Inválido" }),
+  status: z.enum(["Lido", "Lendo", "Quero ler"], { error: "Status Inválido" }),
   stars: z.number(),
   comment: z.string(),
 });
